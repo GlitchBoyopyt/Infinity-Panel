6979cc6bd0c99529a2aea53810dc24cc7073bd36
GlitchBoyopyt <162595849+GlitchBoyopyt@users.noreply.github.com>
2025-02-16 12:43:07 +0530

    Create Login.js

---
+++ src/Login.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const handleLogin = async () => {
+        const res = await axios.post('http://localhost:3000/api/auth/login', { username, password });
+        localStorage.setItem('token', res.data.token);
+        window.location.href = '/dashboard';
+    };
+
+    return (
+        <div>
+            <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+            <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={handleLogin}>Login</button>
+        </div>
+    );
+};
+
+export default Login;
