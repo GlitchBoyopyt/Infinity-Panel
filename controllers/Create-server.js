9d1144df5971fd603f6e124c61b5f21a59d2e19c
GlitchBoyopyt <162595849+GlitchBoyopyt@users.noreply.github.com>
2025-02-22 11:58:23 +0530

    Create Create-server.js

---
+++ controllers/Create-server.js
@@ -0,0 +1,3 @@
+curl -X POST http://localhost:3000/api/servers \
+     -H "Content-Type: application/json" \
+     -d '{"userId": "65b9876def0123abc4567890", "name": "Survival Server", "expiresAt": "2025-02-20T00:00:00.000Z"}'
